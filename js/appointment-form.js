5836a88c772dbe5204b9dea51ac3523dec447c29
Aste <aste@tuta.io>
2024-07-06 17:22:54 +0200

    Add initial working web3forms

--- js/appointment-form.js
+++ js/appointment-form.js
@@ -1,12 +1,83 @@
 const alertParent = document.getElementById("appointment-status");
 const form = document.getElementById("appointment-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const cprInput = document.getElementById("cpr");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+nameInput.addEventListener("input", () => {
+  if (nameInput.validity.tooShort) {
+    nameInput.setCustomValidity("Indtast venligst dit fulde navn");
+  } else {
+    nameInput.setCustomValidity("");
+  }
+});
 
+emailInput.addEventListener("input", () => {
+  if (emailInput.validity.patternMismatch) {
+    emailInput.setCustomValidity("Indtast din email");
+  } else {
+    emailInput.setCustomValidity("");
+  }
+});
+
+phoneInput.addEventListener("input", () => {
+  if (phoneInput.validity.patternMismatch) {
+    phoneInput.setCustomValidity("Indtast dit danske telefonnummer");
+  } else {
+    phoneInput.setCustomValidity("");
+  }
+});
+
+cprInput.addEventListener("input", () => {
+  if (cprInput.validity.patternMismatch) {
+    cprInput.setCustomValidity("Indtast dit fulde CPR-nummer (10 cifre)");
+  } else {
+    cprInput.setCustomValidity("");
+  }
+});
+
+// Format phone number and CPR
+
+
+// Add bootstrap validation styles
+(() => {
+  "use strict";
+  window.addEventListener(
+    "load",
+    () => {
+      let forms = document.getElementsByClassName("needs-validation");
+      let validation = Array.prototype.filter.call(forms, (form) => {
+        form.addEventListener(
+          "submit",
+          (event) => {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            } else {
+              event.preventDefault();
+              submitForm();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+const submitForm = () => {
   const formData = new FormData(form);
   const object = Object.fromEntries(formData);
   const json = JSON.stringify(object);
+
+  let replyto = document.getElementById("replyto");
+  let subject = document.getElementById("subject");
+
+  replyto.value = `${emailInput.value}`;
+  subject.value = `Ny tidsbestilling fra ${nameInput.value}`;
 
   let alertStatus = document.createElement("div");
   alertStatus.classList.add("mt-3");
@@ -41,10 +112,13 @@ form.addEventListener("submit", function (e) {
     })
     .then(() => {
       form.reset();
+      form.classList.remove("was-validated"); // Remove validation styling
+      const invalidFeedbacks = form.querySelectorAll(".invalid-feedback");
+      invalidFeedbacks.forEach((feedback) => (feedback.style.display = "none"));
       setTimeout(() => {
         while (alertParent.hasChildNodes()) {
           alertParent.removeChild(alertParent.firstChild);
         }
       }, 8000);
     });
-});
+};
