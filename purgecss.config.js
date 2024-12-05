8b4554e1e6ae1cea71c36c38b2a8d5855e643247
Aste <aste@tuta.io>
2024-12-05 17:42:34 +0100

    perf(html): reduce loading priority for hidden darkbluelogo

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-// purgecss.config.js
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  css: ["./src/**/*.css"],
-  output: "./src/css/",
-  safelist: {
-    standard: [/swiper-/, /modal-/, /show/, /active/, /collapsed/, /collapse/],
-  },
-};
